090ceadd85b9bd01e70d7f0077d9d5a9bb6b6acc
Sashko Stubailo <sashko@stubailo.com>
2017-03-22 17:55:11 -0700

    Add webpack and a data file

---
+++ src/data.js
@@ -0,0 +1,4 @@
+// This is silly, just a demo!
+module.exports = {
+  companyName: 'Meteor Development Group'
+}
---
+++ src/index.js
@@ -0,0 +1,74 @@
+var $ = window.$;
+
+function init() {
+  // Make all links open in new tab
+  $('a:not(.fragment)').attr('target', '_blank');
+
+  // Display jobs from Lever
+  var $jobsList = $('#jobs-list');
+  var $main = $('#jobs-main');
+  var jobs = [];
+
+  function displayJob(job, $jobsListItem) {
+    $main.empty();
+
+    var $jobTitle = $('<h2>' + job.text + '<span class="icon-link"><span/></h2>');
+
+    $jobTitle.on('click', function () {
+      window.location.hash = job.id;
+    });
+
+    $main.append($jobTitle);
+
+    $main.append('<a href="' + job.applyUrl + '" class="btn">Apply now</a>');
+    $main.append('<div>' + job.description + '</div>');
+
+    job.lists.forEach(function (list) {
+      $main.append('<h3>' + list.text + '</h3>');
+      $main.append('<ul>' + list.content + '</ul>');
+    });
+
+    $main.append('<div>' + job.additional + '</div>');
+
+    $jobsList.find('li').removeClass('active');
+    $jobsListItem.addClass('active');
+  }
+
+  $.getJSON('https://api.lever.co/v0/postings/meteor?mode=json', function (data) {
+    jobs = data;
+
+    jobs = data.filter(job => !job.text.match(/intern/i));
+    var internships = data.filter(job => !!job.text.match(/intern/i));
+    jobs.push(...internships);
+
+    jobs.forEach(function (job, index) {
+      var $jobsListItem = $(
+        '<li><a href="#' + job.id + '">' + job.text + '</a></li>');
+      $jobsList.append($jobsListItem);
+
+      $jobsListItem.on('click', function (e) {
+        e.preventDefault();
+        displayJob(job, $jobsListItem);
+      });
+    });
+
+    var selectedJob = jobs[0];
+    if (window.location.hash) {
+      var id = window.location.hash.substr(1);
+      var jobWithId = jobs.filter(function (job) { return job.id === id });
+
+      if (jobWithId.length) {
+        selectedJob = jobWithId[0];
+      }
+    }
+
+    displayJob(selectedJob, $('#link-' + selectedJob.id).parent());
+
+    if (window.location.hash) {
+      // Only scroll to the job description if we are intentionally linking there
+      document.body.scrollTop = $main.offset().top;
+    }
+  });
+}
+
+$(init);
