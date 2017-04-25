e19281182623f6c6b7f0ae0057cdfa1d2d1b0461
Nick Martin <nim+github@nimlabs.org>
2017-04-25 11:22:25 -0700

    Update Nick's bio

--- src/data.js
+++ src/data.js
@@ -21,7 +21,7 @@ const people = [{
   founder: true,
   title: 'CTO',
   twitter: 'n1mmy',
-  bio: 'Nick has built a wide array of interests over time, ranging from curating perfumes to flying drones and bitcoining (both before they were cool). He\'s our CTO and chief architect across everything we do, as well as our hot sauce sommelier.',
+  bio: 'Nick is a serial hobbiest and has had a wide array of interests over time, from bread baking to perfume making to ham radio and many more. He\'s our CTO and chief architecture whisperer, as well as our hot sauce sommelier.',
 }, {
   name: 'Matt DeBergalis',
   picture: 'matt.jpg',
